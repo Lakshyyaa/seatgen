cf7f998304f6b9fe584caa55faab1b91f4dde3d0
Lakshyyaa <lakshyaasin@gmail.com>
2023-11-20 00:30:04 +0530

    raising halfway done

--- BE/index.js
+++ BE/index.js
@@ -1,85 +1,67 @@
-// FOR SEAT ALLOCATION
-// add two subject students? but need to add time and all
-// have an lt database, with number of rows and collumns lt.xlsx
-// have a staff database, with their name, dep, free/not in mongodb
-// input the subject name and student list xlsx sheet, which will be returned with seats, mail or download
-// rows are from A-Z and collumns form 1-10
-// ADD INVIGILATION IN IT TOO, test case if free teachers not found
-
-
 const xlsx = require('xlsx');
 const fs = require('fs');
 const mongoose = require('mongoose');
 const connectDB = require('./db.js');
 
-
-// The function below fetches the sheet and returns its json to be processed from further
-function getsheet(x) {
+function fetchSheet(x) {
     const workbook = xlsx.readFile(x);
-    const sheetName = workbook.SheetNames[0];
+    const sheetName = workbook.SheetNames[0];   
     const sheet = workbook.Sheets[sheetName];
-    const sheetdata = xlsx.utils.sheet_to_json(sheet);
-    return sheetdata
+    const sheetData = xlsx.utils.sheet_to_json(sheet);
+    return sheetData;
 }
 
-
-// The function below does the invigilationgen and is called in the seatgen function
-// Return false if not enough teachers availabe or if calendar is filled.
-async function invigilation(newdata, subname) {
-    await connectDB()
+async function invigilationGen(newData, subName) {
+    await connectDB();
     const Teacher = mongoose.model('Teacher', {
         name: String,
         subject: String,
         free: Number,
     });
     let teachers = await Teacher.find();
-    const numofteachers = teachers.length
-    let i = 0
+    const numOfTeachers = teachers.length;
+    let i = 0;
     let j = 0;
-    let teachersnotfree=[]
-    for (const element of newdata) {
+    let teachersNotFree = [];
+
+    for (const element of newData) {
         if (i % 20 == 0) {
-            while (j < numofteachers && (teachers[j].subject == subname || teachers[j].free == 0)) {
-                j++
-                console.log('j increased.')
+            while (j < numOfTeachers && (teachers[j].subject == subName || teachers[j].free == 0)) {
+                j++;
+                console.log('j increased.');
             }
-            if (j >= numofteachers) {
+            if (j >= numOfTeachers) {
                 // write code to return denial of request as no teacher available
                 // return 0;
-                // return right thing, to let user know what error/right thing has happend
+                // return right thing, to let the user know what error/right thing has happened
                 break;
-                // putting break right now as i want it to work even for less teachers
+                // putting break right now as I want it to work even for fewer teachers
             }
 
-            teachersnotfree.push(teachers[j]._id)
+            teachersNotFree.push(teachers[j]._id);
             teachers[j].free = 0;
-            element.INVI = teachers[j].name
-        }
-        else {
-            element.INVI = teachers[j].name
+            element.INVI = teachers[j].name;
+        } else {
+            element.INVI = teachers[j].name;
         }
         i++;
     }
-    // below code updated the teachers to not free
-    try{
-        await Teacher.updateMany({ _id: { $in: teachersnotfree } }, { $set: { free: 0 } });
-        console.log('updated db')
+
+    // below code updates the teachers to not free
+    try {
+        await Teacher.updateMany({ _id: { $in: teachersNotFree } }, { $set: { free: 0 } });
+        console.log('updated db');
+    } catch (err) {
+        console.error(err);
     }
-    catch(err)
-    {
-        console.error(err)
-    }
-    writetosheet(newdata, subname)
+    writeToSheet(newData, subName);
     return 1;
-    // return right thing, to let user know what error/right thing has happend
+    // return the right thing, to let the user know what error/right thing has happened
 }
 
-
-// The function below generates diagonal seating plan for each lt and writes to an excel sheet having subject name
-// It stores the object json in newdata array first then writes to the xlsx sheet in the end
-function seatgen(subname, inputstudentlist, ltsheetname) {
-    // delete the Aarrangement file, just to refresh
-    fs.unlink(`${subname}` + 'arrangement.xlsx', (err) => {
+function seatGen(subName, inputStudentList, ltSheetName) {
+    // delete the arrangement file, just to refresh
+    fs.unlink(`${subName}` + 'arrangement.xlsx', (err) => {
         if (err) {
             console.error(`Error deleting arrangement file: ${err}`);
         } else {
@@ -88,59 +70,65 @@ function seatgen(subname, inputstudentlist, ltsheetname) {
     });
 
     // main function starts from here
-    let studentlist = getsheet(inputstudentlist)
-    let numberofstudents = studentlist.length
-    let ltsheet = getsheet(ltsheetname)
-    let newdata = []
-    var studentindex = 0;
-    for (let i = 0; i < ltsheet.length; i++) {
-        var rowsoflt = ltsheet[i].ROW
-        var row = 1
-        while (studentindex < numberofstudents && row <= rowsoflt) {
-            var collumnsoflt = ltsheet[i].COLLUMN
-            var collumn = (row - 1) % 3 + 1
-            while (studentindex < numberofstudents && collumn <= collumnsoflt) {
+    let studentList = fetchSheet(inputStudentList);
+    let numberOfStudents = studentList.length;
+    let ltSheet = fetchSheet(ltSheetName);
+    // HERE WE SEE IF ENOUGH SEATS AVAILABLE TO SEND A BIG FOFF
+    // BUT REMEMBER TO ADD AN AVAILABLE COLUMN TO IT AS WELL 0/1
+    // THAT WILL BE CHANGED JUST LIKE THE TEACHERDB IN THE END OF THIS FUNCTION.
+    let newData = [];
+    var studentIndex = 0;
+
+    for (let i = 0; i < ltSheet.length; i++) {
+        var rowsOfLt = ltSheet[i].ROW;
+        var row = 1;
+
+        while (studentIndex < numberOfStudents && row <= rowsOfLt) {
+            var collumnsOfLt = ltSheet[i].COLLUMN;
+            var collumn = (row - 1) % 3 + 1;
+
+            while (studentIndex < numberOfStudents && collumn <= collumnsOfLt) {
                 // can add the logic here for invigilation
-                studentlist[studentindex].LT = ltsheet[i].LT
-                studentlist[studentindex].SEAT = row + '' + String.fromCharCode(collumn + 64)
-                newdata.push(studentlist[studentindex])
-                studentindex++
+                studentList[studentIndex].LT = ltSheet[i].LT;
+                studentList[studentIndex].SEAT = row + '' + String.fromCharCode(collumn + 64);
+                newData.push(studentList[studentIndex]);
+                studentIndex++;
                 collumn = collumn + 2;
             }
             row++;
         }
-        if (studentindex >= numberofstudents) {
+        if (studentIndex >= numberOfStudents) {
             break;
         }
     }
-    invigilation(newdata, subname);
-    // return right thing, to let user know what error/right thing has happend
+
+    invigilationGen(newData, subName);
+    // return the right thing, to let the user know what error/right thing has happened
     return 1;
 }
 
-
-// The function below writes to sheet we want the data we want
-function writetosheet(newdata, subname) {
+// The function below writes to the sheet we want the data we want
+function writeToSheet(newData, subName) {
     try {
         const wb = xlsx.utils.book_new();
-        const ws_name = "Sheet1";
+        const wsName = "Sheet1";
         // Convert the array of objects to a worksheet
-        const ws = xlsx.utils.json_to_sheet(newdata);
+        const ws = xlsx.utils.json_to_sheet(newData);
         // Add the worksheet to the workbook
-        xlsx.utils.book_append_sheet(wb, ws, ws_name);
+        xlsx.utils.book_append_sheet(wb, ws, wsName);
         // Write the workbook to a file
-        xlsx.writeFile(wb, `${subname}arrangement.xlsx`);
+        xlsx.writeFile(wb, `${subName}arrangement.xlsx`);
         console.log('File written successfully.');
-        // return right thing, to let user know what error/right thing has happend
+        // return the right thing, to let the user know what error/right thing has happened
         return 1;
     } catch (error) {
         console.error('Error writing to file:', error);
-        // return right thing, to let user know what error/right thing has happend
+        // return the right thing, to let the user know what error/right thing has happened
         return 0;
     }
 }
 
-let subname = 'A' // will come from frontend
-let inputstudentlist = 'students.xlsx' // will come from frontend
-let ltsheetname = 'lt.xlsx' // will be stored in backend
-seatgen(subname, inputstudentlist, ltsheetname);
+let subName = 'A'; // will come from frontend
+let inputStudentList = 'students.xlsx'; // will come from frontend
+let ltSheetName = 'lt.xlsx'; // will be stored in backend
+seatGen(subName, inputStudentList, ltSheetName);
--- BE/raise.js
+++ BE/raise.js
@@ -1,12 +1,118 @@
-// A SCHEMA FOR EXAMS SCHEDULED FOR FUTURE: NAME AND TIME AND LT
+// A SCHEMA FOR EXAMS SCHEDULED FOR FUTURE: SUB AND TIME AND LT
 // A SCHEMA FOR THE REQUESTS SENT: SUB, TIME, EXCEL SHEET LINK, TYPE? ANNOUNCE OR SCHEDULE
 
 // UPON LOGIN OF A TEACHER, WHENEVER THE TEACHER TYPES IN THE TIME, THE FRONTEND CHECKS IN THE DB
-// IF THE TEACHER HAS SCHEDULED ONE ALREADY OR IF THE TIME SLOT IS BOOKED AND RETURN FOFF IF SO.
+// 1. IF THE TEACHER HAS SCHEDULED ONE ALREADY AS ONE REQUEST PER TEACHER UNTIL CONCLUDED
+// 2. OR IF THE TIME SLOT IS BOOKED AND RETURN FOFF IF SO OR
+// 3. IF SEATS ENOUGH (WRITE ITS FUNCTIONS WHICH SEES AVAILABLE COLLUMNS IN EXCEL SHEET OF LT)
 // EACH SUB CAN HAVE ONLY ONE EXAM PER TIME AND NO REQUESTS UNTIL NEW OCCURRED.
 
 // THE FRONTEND RECEIVES THE XLSX SHEET AND UPLOADS THEM TO GITHUB, THEN SEND THE DATA AS SCHEMA OBJECT TO BACKEND
 // AND GETS STORED IN MONGODB
 
-// ON OPENING THE ACCOUNT, THE ADMIN CAN VIEW ALL THE REQUESTS
+// ON OPENING THE ACCOUNT, THE ADMIN CAN VIEW ALL THE REQUESTS, THEN TAKE THE ACTION:
+// 1. ANNOUNCEMENT: SEND EMAIL TO THE STUDENTS IN THE SHEET AFTER FETCHING IT, AND WRITE THE E-MAIL
+// 2. CLICK ON THE SEATGEN FUNCTION AND SEND IT ALL AS EMAIL TO THE SAME STUDENTS.
 
+// AFTER THE WORK IS DONE, IT DELETES THE SHEET FROM THE REPO, ADD TO CALENDAR, REMOVE FROM REQUESTS.
+// THE NAME OF THE SHEET IS SUB_STUDENTS
+
+
+const { Octokit } = require('@octokit/rest');
+const fs = require('fs');
+const axios = require('axios');
+const xlsx = require('xlsx')
+
+// Replace with your GitHub personal access token
+const token = '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';
+
+// Create an Octokit instance with authentication
+const octokit = new Octokit({
+    auth: token,
+});
+
+// Replace with your GitHub username, repository name, and branch
+const owner = 'Lakshyyaa';
+const repo = 'emscdn';
+const branch = 'main';
+
+
+async function uploadSheet(filePath) {
+    try {
+        // Read the content of the file
+        const fileContent = fs.readFileSync(filePath, 'base64');
+
+        // Create or update a file in the repository
+        const response = await octokit.repos.createOrUpdateFileContents({
+            owner,
+            repo,
+            path: 'files/' + filePath, // Replace with the desired path in your repository
+            message: 'Upload file', // Commit message
+            content: fileContent, // Convert content to base64
+            branch,
+        });
+        console.log('File uploaded successfully');
+    } catch (error) {
+        console.error('Error uploading file:', error.message);
+    }
+}
+
+
+// Function to delete a file from the repository
+async function deleteSheet(sheetName) {
+    try {
+        // Get the current content of the file
+        const existingFile = await octokit.repos.getContent({
+            owner,
+            repo,
+            path: 'files/' + sheetName, // Adjust the path based on your repository structure
+            ref: branch,
+        });
+
+        // Delete the file
+        const response = await octokit.repos.deleteFile({
+            owner,
+            repo,
+            path: 'files/' + sheetName,
+            message: 'Delete file', // Commit message
+            sha: existingFile.data.sha, // SHA of the existing file
+            branch,
+        });
+
+        console.log('File deleted successfully');
+    } catch (error) {
+        console.error('Error deleting file:', error.message);
+    }
+}
+
+
+// Example usage
+
+
+// Function to fetch the content of a file
+async function fetchSheet(filePath) {
+    const url = `https://api.github.com/repos/Lakshyyaa/emscdn/contents/files/${filePath}`;
+
+    try {
+        const response = await axios.get(url);
+        const base64Content = response.data.content;
+        const fileBuffer = Buffer.from(base64Content, 'base64');
+        const workbook = xlsx.read(fileBuffer, { type: 'buffer' });
+        const sheetName = workbook.SheetNames[0];
+        const sheetData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
+        console.log(sheetData)
+    } catch (error) {
+        console.error('Error fetching sheet:', error.message);
+        throw error;
+    }
+}
+
+
+async function main() {
+    // await uploadSheet('staff.xlsx');
+    // await fetchSheet('staff.xlsx');
+    // await deleteSheet('students.xlsx');
+}
+
+// Run the main function
+main().catch((error) => console.error('Error:', error));
